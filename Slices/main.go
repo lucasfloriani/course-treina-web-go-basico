a89a3a577e4b0dc00803aaeb4f660e5c35c4d6cd
Lucas Floriani <lucas.floriane@gmail.com>
2018-07-14 16:52:33 -0300

    Examples with Slices

---
+++ Slices/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	amigos := make([]string, 3)
+	nome := ""
+	i := 0
+	for nome != "q" {
+		fmt.Print("Digite o nome de um amigo ou 'q' para parar: ")
+		fmt.Scanf("%s", &nome)
+		if nome != "q" {
+			if i < 3 {
+				amigos[i] = nome
+			} else {
+				amigos = append(amigos, nome)
+			}
+			i++
+		}
+	}
+	fmt.Println(amigos)
+
+	fmt.Printf("Você tem %d amigos \n", len(amigos))
+	for _, nomeAmigo := range amigos {
+		fmt.Printf(" - %s \n", nomeAmigo)
+	}
+
+	outroSlice := amigos[1:3]
+	fmt.Println(outroSlice)
+	maisUmSlice := amigos[:3]
+	fmt.Println(maisUmSlice)
+}
